2d67bbcd0b09db119cab27bdf7633b0523e45ce6
gregmalcolm <greg@gregmalcolm.com>
2016-01-13 21:40:08 -0500

    Added spec coverage for GET filters

--- spec/controllers/api/v1/world_surveys_controller_spec.rb
+++ spec/controllers/api/v1/world_surveys_controller_spec.rb
@@ -9,17 +9,53 @@ describe Api::V1::WorldSurveysController, type: :controller do
 
   describe "GET #index" do
     let(:json) { JSON.parse(response.body)["world_surveys"] }
+    let(:commanders) { json.map { |j| j["commander"] } }
 
-    before { get :index, {}, valid_session }
-    it { expect(response).to have_http_status(200) }
-    it { expect(json[0]["system"]).to be == "NGANJI" }
-    it { expect(json.size).to be >= 3 }
+    context "drinking from the firehouse" do
+      before { get :index, {}, valid_session }
+      it { expect(response).to have_http_status(200) }
+      it { expect(json[1]["system"]).to be == "NGANJI" }
+      it { expect(json.size).to be >= 3 }
+    end
+
+    describe "filtering" do
+      context "on system" do
+        before { get :index, {q: {system: " nganjI "}}, valid_session }
+        it { expect(json[0]["system"]).to be == "NGANJI" }
+        it { expect(json.size).to be == 1 }
+      end
+
+      context "on commander" do
+        before { get :index, {q: {commander: "  DOMMAARRAA"}}, valid_session }
+        it { expect(json[0]["system"]).to be == "SHINRARTA DEZHRA" }
+        it { expect(json.size).to be == 1 }
+      end
+
+      context "on world" do
+        before { get :index, {q: {world: "a 5 "}}, valid_session }
+        it { expect(json[0]["system"]).to be == "SHINRARTA DEZHRA" }
+        it { expect(json.size).to be == 2 }
+      end
+
+      context "on updated_before" do
+        before { get :index, {q: {updated_before: Time.now - 3.days}}, valid_session }
+        it { expect(commanders).to include "Finwen" }
+        it { expect(commanders).to include "Marlon Blake" }
+        it { expect(json.size).to be == 2 }
+      end
+
+      context "on updated_after" do
+        before { get :index, {q: {updated_after: Time.now - 3.days}}, valid_session }
+        it { expect(commanders).to include "Dommaarraa" }
+        it { expect(json.size).to be == 1 }
+      end
+    end
   end
 
   describe "GET #show" do
     let(:json) { JSON.parse(response.body)["world_survey"] }
 
-    before { get :show, {id: surveys[1].id}, valid_session }
+    before { get :show, {id: surveys[2].id}, valid_session }
     it { expect(response).to have_http_status(200) }
     it { expect(json["system"]).to be == "SHINRARTA DEZHRA" }
     it { expect(json["commander"]).to be == "Dommaarraa" }
@@ -56,11 +92,11 @@ describe Api::V1::WorldSurveysController, type: :controller do
   end
 
   describe "PATCH/PUT #update" do
-    let(:updated_survey) { { id: surveys[0].id,
+    let(:updated_survey) { { id: surveys[1].id,
                              world_survey: { mercury: true }} }
     context "updating a survey" do
       before { put :update, updated_survey, valid_session }
-      let(:survey) { WorldSurvey.find(surveys[0].id) }
+      let(:survey) { WorldSurvey.find(surveys[1].id) }
       it { expect(response).to have_http_status(204) }
       it { expect(survey.system).to be == "NGANJI" }
       it { expect(survey.mercury).to be true }
