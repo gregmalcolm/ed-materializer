7a16cd8dd930f4c760f5bda6631e9aa47d6aed22
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-01-18 16:06:34 -0500

    Devise was breaking my tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, :type => :controller
 end
