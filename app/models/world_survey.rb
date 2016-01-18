cc1c5bd59b9845a0128fb7706a77033dc6ed4fd7
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-01-18 15:52:18 -0500

    Minor tweaks

--- app/models/world_survey.rb
+++ app/models/world_survey.rb
@@ -3,7 +3,7 @@ class WorldSurvey < ActiveRecord::Base
 
   scope :by_system,    ->(system)    { where("UPPER(TRIM(system))=?", system.to_s.upcase.strip ) if system }
   scope :by_commander, ->(commander) { where("UPPER(TRIM(commander))=?", commander.to_s.upcase.strip ) if commander }
-  scope :by_worl<F6>,     ->(world)     { where("UPPER(TRIM(world))=?", world.to_s.upcase.strip ) if world }
+  scope :by_world,     ->(world)     { where("UPPER(TRIM(world))=?", world.to_s.upcase.strip ) if world }
 
   scope :not_me, ->(id) { where.not(id: id) if id }
 
