703df7e1fa48ea75aa8229b7b1c52a6ba552f137
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-01-18 14:19:55 -0500

    protect_from_forgery doesn't appear to be present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   def page
     params[:page] || 1
