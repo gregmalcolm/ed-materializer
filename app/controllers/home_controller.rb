8b1f3643683ef2fd42e398407534340575041b25
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-02-27 19:18:41 -0500

    Update front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render text: "Elite Dangerous Materializer"
+    render text: "<h3>Elite Dangerous Materializer API</h3><a href='https://github.com/gregmalcolm/ed-materializer/wiki/Schema'>Schema</a>"
   end
 end
