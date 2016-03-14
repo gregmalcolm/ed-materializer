edbc7e8dc93a19289e3d9b0ecd7877c1ecf248b4
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-03-14 00:41:53 -0400

    Mailer tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :rememberable,
          :trackable,
          :validatable,
-         #:confirmable,
+         :confirmable,
          :omniauthable
   include DeviseTokenAuth::Concerns::User
 end
