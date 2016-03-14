ec292565361a504ac279ce9ce412db1acc0cfa0c
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-03-14 13:04:02 -0400

    Updating mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.smtp_settings = {
-    #address:              'smtp.gmail.com',
-    #port:                 587,
-    #domain:               ENV["MAILER_DOMAIN"],
-    #user_name:            ENV["GM_USER"],
-    #password:             ENV["GM_PASS"],
-    #authentication:       'plain',
-    #enable_starttls_auto: true  }
-
   config.action_mailer.default_url_options = { :host => ENV["MAILER_HOST"] }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = false  
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
