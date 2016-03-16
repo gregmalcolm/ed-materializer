89ba6445eef98997bebdf2376a495d256b0571fe
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-03-15 23:39:37 -0400

    Put together a crude way to download snapshots

--- app/controllers/concerns/data_dump_actions.rb
+++ app/controllers/concerns/data_dump_actions.rb
@@ -1,17 +1,34 @@
 module DataDumpActions
   def download
-    @model_name = controller_name.classify
-    send_file("#{Rails.root}/exports/snapshots/#{filename_base}.json.zip")
+    ensure_download_exists
+    send_file("#{full_json_filename}.zip")
   end
 
   def md5
-    md5 = File.read("#{Rails.root}/exports/snapshots/#{filename_base}.json.md5")
-    render json: {md5sum: md5} 
+    ensure_download_exists
+    md5 = File.read("#{full_json_filename}.md5")
+    render json: { controller_name => { md5: md5 }}
   end
 
   private
 
+  def full_json_filename
+    "#{Rails.root}/exports/snapshots/#{filename_base}.json"
+  end
+  
   def filename_base
     controller_name.dasherize
+  end
+
+  def ensure_download_exists
+    zipfile = "#{full_json_filename}.zip"
+    if !File.exists?(zipfile) || expired?
+      `rake export:snapshot:#{controller_name}`
+    end
+  end
+
+  def expired?
+    file_date = File.mtime("#{full_json_filename}.zip").to_date
+    file_date != Date.today
   end
 end
